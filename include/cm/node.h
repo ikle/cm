76588d8982c7bb091d6c679aa520590603e519c2
Alexei A. Smekalkine <ikle@ikle.ru>
2017-07-23 18:27:44 +0300

    cm-node: reorder interface functions, no functional changes

--- include/cm/node.h
+++ include/cm/node.h
@@ -38,16 +38,6 @@ const char *cm_node_pop (struct cm_node *o);
 int cm_node_push_list (struct cm_node *o, char *argv[]);
 
 /*
- * The cm_node_read pushes NULL-terminated sequence of values into the
- * specified node stack.
- *
- * If the last value equal * (star character) then it reads real value
- * from the configuration and pushes it on top of stack.
- */
-int cm_node_read (const char *conf, struct cm_node *o,
-		  const char *node, ...) sentinel;
-
-/*
  * The cm_node_print function returns the node path representation
  * constructed using the sep as node separator. If separator is space
  * character then node value will be quoted.
@@ -66,5 +56,15 @@ const char *cm_node_print (struct cm_node *o, int sep);
  * buffer overflow.
  */
 int cm_node_validate (const char *conf, struct cm_node *o);
+
+/*
+ * The cm_node_read pushes NULL-terminated sequence of values into the
+ * specified node stack.
+ *
+ * If the last value equal * (star character) then it reads real value
+ * from the configuration and pushes it on top of stack.
+ */
+int cm_node_read (const char *conf, struct cm_node *o,
+		  const char *node, ...) sentinel;
 
 #endif  /* CM_NODE_H */
